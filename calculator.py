16da88e9628b7214b10878d82dce99831645123f
Daniel Melendez <danmelendez1986@gmail.com>
2025-09-17 19:12:19 -0300

    modifying calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,3 @@
-input1 = int(input("enter number 1: "))
-math_operation = input(("enter operator:  "))
-input2 = int(input("enter number 2: "))
-
 def sum(input1, input2):
         return input1 + input2
 def substract(input1, input2):
@@ -10,6 +6,23 @@ def multiply(input1, input2):
         return input1 * input2
 def divide(input1, input2):
         return input1 / input2
+"""
+def menu():
+        print("Bienvenido a la calculadora de jose")
+        print("1. suma")
+        print("2. resta")
+        print("3. Multiplicacion")
+        print("4. division")
+        
+        
+
+def calculator():
+        menu()
+
+"""
+input1 = float(input("enter number 1: "))
+math_operation = input(("enter operator '+ , -, *, /:  "))
+input2 = float(input("enter number 2: "))
 
 
 if math_operation == "+":
@@ -25,3 +38,15 @@ elif math_operation == "/":
         print("cannot divide by 0")
 else:
     print("invalid")
+
+
+
+"""
+if __name__ == "__calculator___":  #llama a main
+        print("hola estoy en el if")
+"""
+
+
+##### estudiar importar archivos y modos en python
+
+### match is the same as switch in Java
